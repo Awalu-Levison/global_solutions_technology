711c6596328ad4af5804449c594949ae78f3dfbd
awalu2 <levisonawalu251@gmail.com>
2026-02-27 17:13:46 +0200

    Updated: Main index file, CSS file and JS file

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Computer Wizard");
+document.getElementById("content").innerHTML = "Hello there!! This is a Computer Wizard Lets get things done";
+
+document.getElementById("myfoot").innerHTML = "This is my fooot section";
+document.querySelector("#myfoot").style.color = "magenta";
